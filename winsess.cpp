dcfba1a2cc6161b4f844e9230684c9d13f1e3732
gregzakh <grishanz@yandex.com>
2023-09-01 23:57:10 +0500

    M winsess.cpp

--- winsess.cpp
+++ winsess.cpp
@@ -9,6 +9,46 @@ auto tfget(std::function<VOID(PLARGE_INTEGER, PTIME_FIELDS)> f, LONGLONG delta) 
   return tf;
 }
 
+void psenumsess(ULONG id) {
+  SYSTEM_SESSION_PROCESS_INFORMATION sspi{};
+  std::vector<BYTE> buf(0x1000);
+  sspi.SessionId = id;
+  sspi.SizeOfBuf = buf.size();
+  sspi.Buffer    = &buf[0];
+
+  ULONG req = 0;
+  NTSTATUS nts = ::NtQuerySystemInformation(
+    SystemSessionProcessInformation, &sspi, sizeof(sspi), &req
+  );
+  if (STATUS_INFO_LENGTH_MISMATCH != nts) {
+    getlasterror(::RtlNtStatusToDosError(nts));
+    return;
+  }
+
+  buf.resize(req);
+  sspi.SizeOfBuf = buf.size();
+  sspi.Buffer    = &buf[0];
+  nts = ::NtQuerySystemInformation(
+    SystemSessionProcessInformation, &sspi, sizeof(sspi), &req
+  );
+  if (!NT_SUCCESS(nts)) {
+    std::vector<BYTE> ().swap(buf);
+    getlasterror(::RtlNtStatusToDosError(nts));
+    return;
+  }
+
+  auto adr = &buf[0];
+  auto spi = reinterpret_cast<PSYSTEM_PROCESS_INFORMATION>(adr);
+  while (spi->NextEntryOffset) {
+    printf("\t\t%wZ (%llu)\n",
+      spi->ImageName, reinterpret_cast<ULONG_PTR>(spi->UniqueProcessId)
+    );
+    adr += spi->NextEntryOffset;
+    spi = reinterpret_cast<PSYSTEM_PROCESS_INFORMATION>(adr);
+  }
+  std::vector<BYTE> ().swap(buf);
+}
+
 int wmain(void) {
   std::locale::global(std::locale(""));
   auto bsz{0x100ul}, items{0ul}, bytes{0ul};
@@ -31,6 +71,7 @@ int wmain(void) {
   printf("System boot time: ");
   tfget(::RtlTimeToTimeFields, vals[1] - vals[2] - vals[0]).gettime();
 
+  ULONG id{};
   for (auto i = 0; i < items; i++) {
     printf("\tSession %wZ starts at ", &buf[i].Name);
     NtObject tmp(::NtOpenSymbolicLinkObject, bnolinks + L"\\" + buf[i].Name.Buffer);
@@ -41,6 +82,12 @@ int wmain(void) {
       continue;
     }
     tfget(::RtlTimeToTimeFields, obi.CreationTime.QuadPart - vals[2]).gettime();
+    nts = ::RtlUnicodeStringToInteger(&buf[i].Name, 0, &id);
+    if (!NT_SUCCESS(nts)) {
+      getlasterror(::RtlNtStatusToDosError(nts));
+      continue;
+    }
+    psenumsess(id);
   }
 
   std::vector<OBJECT_DIRECTORY_INFORMATION> ().swap(buf);
